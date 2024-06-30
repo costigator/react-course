cbc7c691156afb93dd0a11f3ca5ba6bbc39d5a7b
costigator <costigator@gmail.com>
2024-06-30 19:10:48 +0200

    hero code updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { ChangeEvent, useState } from "react"
+
+type Hero = {
+  id: number;
+  name: string;
+}
+
 export default function App() {
+
+  const [hero, setHero] = useState<Hero>({id: 1, name: 'Superman'})
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setHero({...hero, name: event.target.value})
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container mt-5 mx-auto">
+      <h2 className="text-2xl">Details</h2>
+      <div>
+        <span className="font-bold">ID:</span> {hero.id}
+      </div>
+      <div className="space-x-2">
+        <span className="font-bold">Name:</span>
+        <span className="uppercase">{hero.name}</span>
+      </div>
+      <div className="flex flex-col gap-2 mt-3 border-t">
+        <label>Hero name</label>
+        <input
+          type="text"
+          placeholder="name"
+          className="border border-gray-300 rounded-lg p-2 w-1/4"
+          value={hero.name}
+          onChange={handleNameChange}
+          />
+      </div>
+    </div>
   )
 }
